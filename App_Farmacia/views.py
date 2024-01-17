1123d5eae76ebab87560e3c99c59e809f337a7ce
Elvis Zhune <elfabri28@gmail.com>
2024-01-17 14:40:06 +0100

    Mejorado

--- App_Farmacia/views.py
+++ App_Farmacia/views.py
@@ -11,4 +11,4 @@ def productos_lista_api(request):
     response = requests.get('http://127.0.0.1:8000/api/v1/productos')
     # Transformamos la respuesta en json
     productos = response.json()
-    return render(request, 'producto/lista_api_cliente.html', {'productos': productos})
+    return render(request, 'producto/lista_api_mejorado.html', {'productos': productos})
