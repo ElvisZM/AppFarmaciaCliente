b32c5850d77416d2ccbc52003067127b662e24c9
Elvis Zhune <elfabri28@gmail.com>
2024-01-23 10:09:54 +0100

    Creando variable de entorno

--- App_Farmacia/urls.py
+++ App_Farmacia/urls.py
@@ -4,5 +4,6 @@ from .import views
 urlpatterns = [
     path('',views.index,name='index'),
     path('productos/lista/api',views.productos_lista_api, name='lista_productos_api'),
+    path('productos/busqueda_simple', views.producto_busqueda_simple, name='productos_busqueda_simple'),
        
 ]
--- App_Farmacia/views.py
+++ App_Farmacia/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render, redirect
+from .forms import *
+
+import requests
+import environ
+import os
+from pathlib import Path
+
+
 
 import requests
 from django.core import serializers
@@ -6,9 +14,20 @@ from django.core import serializers
 def index(request):
     return render(request, 'index.html')
 
+def crear_cabecera():
+    return {'Authorization': 'Bearer '+env("TOKEN_ACCESO")}
+
 def productos_lista_api(request):
     # Obtenemos todos los productos
-    response = requests.get('http://127.0.0.1:8000/api/v1/productos')
+    headers = {'Authorization': 'Bearer SHleaIZhlO8DDpayPQ7pgNiIPu9ZDz'}
+    response = requests.get('http://127.0.0.1:8000/api/v1/productos',headers=headers)
     # Transformamos la respuesta en json
     productos = response.json()
     return render(request, 'producto/lista_api_mejorado.html', {'productos': productos})
+
+def producto_busqueda_simple(request):
+    formulario = BusquedaProductoForm(request.GET)
+    
+    if formulario.is_valid():
+        headers = crear_cabecera()
+        response = request.get
